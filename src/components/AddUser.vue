0781cdcc4640723ec8e9c61b305ef1c86e422922
kirillNovoseletskii <48125494+kirillNovoseletskii@users.noreply.github.com>
2021-06-14 13:47:57 +0300

    comm

--- src/components/AddUser.vue
+++ src/components/AddUser.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="modal-main" @keydown.esc="exit">
     <div class="modal-content">
+      <button class="close" @click="TOGGLE_MODE(false)">
+        <i class="icon-close"></i>
+      </button>
       <input type="text" placeholder="@username" class="input" v-model="username">
       <input type="text" placeholder="Full Name" class="input" v-model="fullname">
       <input type="text" placeholder="your@email.com" class="input" v-model="email">
@@ -229,7 +232,6 @@ export default {
           }
         }
   }
-
   .save-btn {
     width: 100%;
     height: 32px;
@@ -246,6 +248,29 @@ export default {
     }
     &:disabled {
       opacity: 0.5;
+    }
+  }
+  .close {
+    width: 24px;
+    height: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0;
+    background: white;
+    border: none;
+    margin-left: auto;
+    margin-top: -12px;
+    .icon-close {
+      width: 24px;
+      height: 24px;
+      background-color: rgb(212, 212, 212);
+      mask-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M18 7.05L16.95 6L12 10.95L7.05 6L6 7.05L10.95 12L6 16.95L7.05 18L12 13.05L16.95 18L18 16.95L13.05 12L18 7.05Z' fill='%23181C2D' stroke='%23181C2D' stroke-width='0.3'/%3E%3C/svg%3E");
+    }
+    &:hover {
+      .icon-close {
+        background-color: rgb(163, 163, 163);
+      }
     }
   }
 </style>
